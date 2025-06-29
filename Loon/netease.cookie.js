e575c345351355333bc085d31500029f0cdb3263
Junsang2000 <95688805+Junsang2000@users.noreply.github.com>
2025-06-29 23:01:42 +0800

    更新 netease.cookie.js

--- Loon/netease.cookie.js
+++ Loon/netease.cookie.js
@@ -1,10 +1,9 @@
-/**
- * 网易云音乐 Cookie 获取脚本
- */
-const cookie = $request.headers.Cookie;  // 获取请求头中的 Cookie 字段
-if (cookie) {
-  $notify("🎧 网易云音乐 Cookie 已获取", "", cookie);
-  // 将 Cookie 存储在 persistent store 中，后续脚本可用
-  $persistentStore.write(cookie, "music_163_cookie");
+// neatease.cookie.js
+if (typeof $request !== 'undefined') {
+  const cookie = $request.headers.Cookie;
+  if (cookie) {
+    $notify('网易云 Cookie 获取成功', '', cookie);
+    $persistentStore.write(cookie, 'music_163_cookie');
+  }
 }
 $done({});
